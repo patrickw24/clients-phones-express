89a95d61dd492c549c7defb1e990ffc21af02b43
patrickw24 <patrick.warner24@gmail.com>
2025-02-07 21:11:04 -0600

    cors added

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express()
 app.use(express.json())
 import { phone } from './routes/phoneRoutes.js'
 import { client } from './routes/clientRoutes.js'
+import cors from 'cors'
 
+app.use(cors())
 app.use(phone)
 app.use(client)
 
